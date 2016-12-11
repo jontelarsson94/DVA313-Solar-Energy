c5addbfb8cd7b4984098dd9087e56e3266eb724a
aliyahussain <syeda.aliya.hussain@gmail.com>
2016-12-11 15:47:40 +0100

    calculated indata-41

---
+++ Example for Login and Design/ctrl/aliyah.js
@@ -0,0 +1,34 @@
+
+
+function Cal_extended_25() {
+    alert("check indata-18 value" + $("#indata-18").val());
+    var p18 = $("#indata-18").val();
+    if (p18 <= 10) {
+        if (($("#personToggle > label").hasClass("active"))) {
+            $("#extended-25").val(3000 * 1.25);
+        } else {
+            $("#extended-25").val(3000);
+        }
+    }
+    else if (p18 <= 30) {
+        if (($("#personToggle > label").hasClass("active"))) {
+            $("#extended-25").val(2000 * 1.25);
+        } else {
+            $("#extended-25").val(2000);
+        }
+    } else if (p18 <= 100) {
+        if (($("#personToggle > label").hasClass("active"))) {
+            $("#extended-25").val(1500 * 1.25);
+        } else {
+            $("#extended-25").val(1500);
+        }
+
+    } else {
+        if (($("#personToggle > label").hasClass("active"))) {
+            $("#extended-25").val(1000 * 1.25);
+        } else {
+            $("#extended-25").val(1000);
+        }
+
+    }
+}
