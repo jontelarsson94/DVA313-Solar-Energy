2c16676749b73dc84fb42b2f77d840efc57cae03
SiNASTON <sebbestune@icloud.com>
2016-12-11 12:55:56 +0100

    sebastian.js complete

--- Example for Login and Design/ctrl/sebastian.js
+++ Example for Login and Design/ctrl/sebastian.js
@@ -10,8 +10,8 @@
 
 //Column Q 
 //If Ekonomisk Livslängd > Ai then Qi = Q(i-1)+Pi
-//Should be updated if there are any changes to Ekonomisk Livslängd or there is any change in P
-$('#calculations').on('keyup', '#indata-24', function(ev){
+//Should be updated if there are any changes to Ekonomisk Livslängd or there is any change in P (this solution should cover all cases)
+$('#calculations').on('change', '#indata-24, #p-4', function(ev){
 
     $("#q-4").val(  parseInt($("#p-4").val()) );
 
@@ -24,7 +24,7 @@ $('#calculations').on('keyup', '#indata-24', function(ev){
 //Column R
 //Ri = SUM(Fi:Mi)
 //Should be updated if there is any change to any of the fields in F:M
-$('#calculations').on('keyup', '#indata-24', function(ev){
+$('#calculations').on('change', '#f-4,f-5,f-6,f-7,f-8,f-9,f-10,f-11,f-12,f-13,f-14,f-15,f-16,f-17,f-18,f-19,f-20,f-21,f-22,f-23,f-24,f-25,f-26,f-27,f-28,f-29,f-30,f-31,f-32,f-33,f-34,f-35,f-36,f-37,f-38,f-39,f-40,f-41,f-42,f-43,f-44,f-45,f-46,f-47,f-48,f-49,f-50,f-51,f-52,f-53,f-54, g-4, h-4, i-4, j-4, k-4, l-4, m-4', function(ev){
 
     var r55 = 0;
 
@@ -38,7 +38,7 @@ $('#calculations').on('keyup', '#indata-24', function(ev){
 //Column S
 //If Ekonomisk Livslängd > Ai then Si = S(i-1)+Ri
 //Should be updated if there are any changes to Ekonomisk Livslängd or there is any change in R
-$('#calculations').on('keyup', '#indata-24', function(ev){
+$('#calculations').on('change', '#indata-24, #r-4', function(ev){
 
     $("#s-4").val(  parseInt($("#r-4").val()) );
 
