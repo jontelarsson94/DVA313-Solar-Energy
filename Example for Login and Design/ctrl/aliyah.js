d6552f2192ac22b7ab82e277831ba2afbd2758a0
aliyahussain <syeda.aliya.hussain@gmail.com>
2016-12-20 02:20:30 +0100

    calculated and tested D83, D88 , D93

---
+++ Example for Login and Design/ctrl/aliyah.js
@@ -0,0 +1,56 @@
+//calculate INtada and result D83 col
+
+function calculateD83() {
+    var count = 0;
+    for (var i = 5; i < 55; i++) {
+        if (parseInt($("#o-" + i).val()) < 0) {
+            count = count + 1;
+        }
+    }
+    if (count < parseInt($("#indata-24").val())) {
+        var val = count + 1;
+        $("#indata-83").val(val);
+    } else {
+        var ErrorStr = ">Livslängd ('" + parseInt($("#indata-24").val()) + "')år";
+        $("#indata-83").val(ErrorStr);
+    }
+
+
+}
+
+//calculate INtada and result D88 col
+function calculateD88() {
+    var count = 0;
+    for (var i = 5; i < 55; i++) {
+        if (parseInt($("#q-" + i).val()) < 0) {
+            count = count + 1;
+        }
+    }
+    if (count < parseInt($("#indata-24").val())) {
+        var val = count + 1;
+        $("#indata-88").val(val);
+    } else {
+        var ErrorStr = ">Livslängd ('" + parseInt($("#indata-24").val()) + "')år";
+        $("#indata-88").val(ErrorStr);
+    }
+
+
+}
+
+//calculate INtada and result D93 col
+function calculateD93() {
+    var count = 0;
+    for (var i = 5; i < 55; i++) {
+        if (parseInt($("#s-" + i).val()) < 0) {
+            count = count + 1;
+        }
+    }
+    if (count < parseInt($("#indata-24").val())) {
+        var val = count + 1;
+        $("#indata-93").val(val);
+    } else {
+        var ErrorStr = ">Livslängd ('" + parseInt($("#indata-24").val()) + "')år";
+        $("#indata-93").val(ErrorStr);
+    }
+
+}
