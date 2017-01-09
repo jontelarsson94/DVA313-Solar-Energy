c2aabc59eb3553df38b39381bbed1b4fcc3e3657
jontelarsson94 <jonathan-larsson@outlook.com>
2017-01-09 14:31:43 +0100

    some fixes for swedish language

--- Example for Login and Design/index.php
+++ Example for Login and Design/index.php
@@ -67,7 +67,7 @@ require_once "src/action/form.php";
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header page-scroll">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                    <span class="sr-only">Toggle navigation</span> Meny <i class="fa fa-bars"></i>
                 </button>
                 <a class="navbar-brand page-scroll" href="#page-top">Future Energy Center</a>
             </div>
@@ -111,7 +111,7 @@ require_once "src/action/form.php";
                         <span class='glyphicon glyphicon-user' aria-hidden='true'></span> Log-out</a>";
                       }else {
                         echo "<a href='' data-toggle='modal' class='page-scroll' data-target='#loginModal'>";
-                        echo "<span class='glyphicon glyphicon-user' aria-hidden='true'></span> Sign in</a>";
+                        echo "<span class='glyphicon glyphicon-user' aria-hidden='true'></span> Logga in</a>";
                       }
                       ?>
                     </li>
@@ -127,7 +127,7 @@ require_once "src/action/form.php";
     <header>
         <div class="container">
             <div class="intro-text">
-                <div class="intro-lead-in">Renewable Energy</div>
+                <div class="intro-lead-in">Förnybar Energi</div>
                 <div class="intro-heading">Solar Economy Calculator</div>
                 <a href="#services" class="page-scroll btn btn-xl">Börja räkna nu</a>
             </div>
@@ -144,24 +144,24 @@ require_once "src/action/form.php";
           <div class="row">
               <div class="col-lg-12 text-center" id="changeDefaultsAdmin">
               <br>
-                  <h2 class="section-heading">Change Default Values</h2>
+                  <h2 class="section-heading">Ändra standardvärden</h2>
                   <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
               </div>
           </div>
 
           <form class="form-group" action="api/upload_excel.php" method="post" enctype="multipart/form-data">
-            Select file to upload:
+            Välf fil att ladda upp:
             <input class="form-control-file" type="file" name="fileToUpload" id="fileToUpload">
             <br>
             <input class="btn btn-primary" type="submit" value="Upload Defaults" name="submit">
           </form>
           <br>
           <form method="get" action="excel/start_default.xlsx">
-            <button class="btn btn-info" type="submit">Download Template</button>
+            <button class="btn btn-info" type="submit">Ladda ner mall</button>
           </form>
           <br>
           <form method="get" action="excel/default_solar.xlsx">
-            <button class="btn btn-info" type="submit">Download Current Excel sheet</button>
+            <button class="btn btn-info" type="submit">Ladda ner nuvarande excel fil</button>
           </form>
         <?php
         }
@@ -604,8 +604,8 @@ require_once "src/action/form.php";
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Contact Us</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h2 class="section-heading">Kontakta oss</h2>
+                    <!--<h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>-->
                 </div>
             </div>
             <div class="row">
@@ -614,28 +614,28 @@ require_once "src/action/form.php";
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Your Name *" id="name" required data-validation-required-message="Please enter your name.">
+                                    <input type="text" class="form-control" placeholder="Ditt namn *" id="name" required data-validation-required-message="Please enter your name.">
                                     <p class="help-block text-danger"></p>
                                 </div>
                                 <div class="form-group">
-                                    <input type="email" class="form-control" placeholder="Your Email *" id="email" required data-validation-required-message="Please enter your email address.">
+                                    <input type="email" class="form-control" placeholder="Din email *" id="email" required data-validation-required-message="Please enter your email address.">
                                     <p class="help-block text-danger"></p>
                                 </div>
                                 <div class="form-group">
-                                    <input type="tel" class="form-control" placeholder="Your Phone *" id="phone" required data-validation-required-message="Please enter your phone number.">
+                                    <input type="tel" class="form-control" placeholder="Ditt telefonnummer *" id="phone" required data-validation-required-message="Please enter your phone number.">
                                     <p class="help-block text-danger"></p>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <textarea class="form-control" placeholder="Your Message *" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                                    <textarea class="form-control" placeholder="Ditt meddelande *" id="message" required data-validation-required-message="Please enter a message."></textarea>
                                     <p class="help-block text-danger"></p>
                                 </div>
                             </div>
                             <div class="clearfix"></div>
                             <div class="col-lg-12 text-center">
                                 <div id="success"></div>
-                                <button type="submit" class="btn btn-xl">Send Message</button>
+                                <button type="submit" class="btn btn-xl">Skicka meddelande</button>
                             </div>
                         </div>
                     </form>
@@ -648,22 +648,21 @@ require_once "src/action/form.php";
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <span class="copyright">Copyright &copy; Your Website 2016</span>
+                    <span class="copyright">Copyright &copy; Future Energy Center 2016</span>
                 </div>
                 <div class="col-md-4">
                     <ul class="list-inline social-buttons">
-                        <li><a href="#"><i class="fa fa-twitter"></i></a>
+                        <!--<li><a href="#"><i class="fa fa-twitter"></i></a>
+                        </li>-->
+                        <li><a href="https://www.facebook.com/futureenergymdh/?fref=ts"><i class="fa fa-facebook"></i></a>
                         </li>
-                        <li><a href="#"><i class="fa fa-facebook"></i></a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                        </li>
+                        <!--<li><a href="#"><i class="fa fa-linkedin"></i></a>
+                        </li>-->
                     </ul>
                 </div>
                 <div class="col-md-4">
                     <ul class="list-inline quicklinks">
-                        <li><a href="" data-toggle='modal' data-target="#privacy_policy">Privacy Policy</a>
-                        </li>
+                        
                         <!--
                         <li><a href="" data-toggle='modal' data-target="#portfolioModal1">Terms of Use</a>
                         </li>
